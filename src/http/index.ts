2e225c2d13dc70e67a9a908ba24da7081df1c0e7
salangdung-i <dladmswl92@naver.com>
2022-08-01 15:52:41 +0900

    feat: http 모듈 구현

---
+++ src/http/index.ts
@@ -0,0 +1,25 @@
+import axios, { AxiosInstance, AxiosResponse } from 'axios';
+
+const BASE_URL = 'http://localhost:8000';
+
+class HttpClient {
+  private httpClient: AxiosInstance;
+  constructor() {
+    this.httpClient = axios.create({
+      baseURL: BASE_URL,
+    });
+  }
+
+  async get<T>(url: string) {
+    const response: AxiosResponse<T> = await this.httpClient.get(url);
+    return response.data;
+  }
+
+  patch<T>(url: string, data: T) {
+    return this.httpClient.post(url, data);
+  }
+}
+
+const httpClient = new HttpClient();
+
+export default httpClient;
