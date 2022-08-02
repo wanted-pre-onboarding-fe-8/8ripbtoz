779cf6453743ffbdfb3f4c3021f81f1dbdc94eab
corgi-world <developer.msw@gmail.com>
2022-08-02 17:13:57 +0900

    refactor: desktop 폴더 생성 후 기존 컴포넌트들 이동

---
+++ src/pages/main/search/desktop/Count.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import styled from 'styled-components';
+import PermIdentityOutlinedIcon from '@mui/icons-material/PermIdentityOutlined';
+import { usePopup, Popup } from '../Popup';
+
+export default function Count() {
+  const { isOpen, open, close } = usePopup();
+
+  return (
+    <Wrapper onMouseDown={open}>
+      <Contents>
+        <PermIdentityOutlinedIcon fontSize='large' />
+        <CountWrapper>
+          <CountTitle>객실 / 인원</CountTitle>
+          <CountValue>객실 1, 인원 2</CountValue>
+        </CountWrapper>
+      </Contents>
+      <Popup top={60} left={0} isOpen={isOpen} close={close}>
+        <PopupTest>인원</PopupTest>
+      </Popup>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  position: relative;
+  flex: 0.6;
+`;
+
+const Contents = styled.div`
+  display: flex;
+  align-items: center;
+  height: 100%;
+  padding: 10px;
+  cursor: pointer;
+
+  :hover {
+    background-color: #eeeeee;
+    * {
+      background-color: #eeeeee;
+    }
+  }
+`;
+
+const CountWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 15px;
+`;
+
+const CountTitle = styled.p`
+  font-size: 12px;
+  margin-bottom: 10px;
+  color: #bdbfc3;
+`;
+
+const CountValue = styled.p`
+  font-size: 16px;
+  font-weight: 600;
+`;
+
+const PopupTest = styled.div`
+  background-color: lightcoral;
+  width: 300px;
+  height: 300px;
+`;
