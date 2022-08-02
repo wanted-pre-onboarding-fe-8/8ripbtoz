5197e2ca6fb7aa707345050f4c5e0291e37469cb
bebusl <bebus1998@naver.com>
2022-08-02 12:00:12 +0900

    style: 글자가 박스를 벗어날 경우 ...으로 표시하도록 수정

--- src/pages/main/Card.tsx
+++ src/pages/main/Card.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Typography } from '@mui/material';
 import styled from 'styled-components';
 import GridCard from './GridCard';
 
@@ -30,8 +29,7 @@ function Card({ hotelName, occupancy, disabled }: CardProps) {
 
 export default Card;
 
-const mockImg = 'https://i.travelapi.com/hotels/1000000/30000/21800/21727/3e65898e_b.jpg';
-
+const mockImg = 'https://i.travelapi.com/hotels/1000000/30000/25000/24908/4a391ebd_b.jpg';
 const Container = styled(GridCard)<{ disabled: boolean }>`
   color: ${({ disabled }) => (disabled ? 'lightGray' : 'inherit')};
   filter: ${({ disabled }) => disabled && 'grayscale(100%)'};
@@ -58,6 +56,15 @@ const Img = styled.img`
 const Info = styled.div`
   grid-area: title;
   padding: 10px;
+`;
+
+const Typography = styled.p<{ fontWeight?: string; fontSize?: string; color?: string }>`
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  font-size: ${({ fontSize }) => (fontSize ? fontSize : '1rem')};
+  font-weight: ${({ fontWeight }) => (fontWeight ? fontWeight : '1rem')};
+  color: ${({ color }) => color};
 `;
 
 const ReservationButton = styled.button`
