1f2f750bfdd2bf700504e170160652f9c5b1bbcb
ohtaekwon <ohtk1119@gmail.com>
2022-08-02 16:49:11 +0900

    feat: 0일떄 비활성 처리

--- src/components/guestselect/GuestSelectButton.tsx
+++ src/components/guestselect/GuestSelectButton.tsx
@@ -1,37 +1,41 @@
-import React, { EventHandler } from 'react';
-import styled, { css } from 'styled-components';
+import React from 'react';
+import styled from 'styled-components';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 import { GUEST } from '../../utils/constants/guest';
 
 interface GuestSelectButtonProps {
   name?: string;
-  count: any;
+  count: number;
   item: string;
   handleChange: (key: string, item: string) => void;
   disabled?: boolean;
 }
 
 export default function GuestSelectButton({ ...props }: GuestSelectButtonProps) {
-  const { count, handleChange, item, disabled } = props;
-  const { ADULT } = GUEST;
-  const adultCount = count.adult;
-  const chidCount = count.child;
+  const { count, handleChange, item } = props;
+  const { ADULT, DECREASE } = GUEST;
 
   return (
     <>
-      {ButtonGroup.map((button) => (
+      {ButtonGroup.map((button, index) => (
         <>
           <ButtonGroups key={button.key} style={{ display: 'flex' }}>
             <Button
               name={button.key}
-              disabled={disabled}
+              disabled={button.key === DECREASE && count === 0}
               onClick={() => handleChange(button.key, item)}
             >
               {button.value}
             </Button>
           </ButtonGroups>
-          {item === ADULT ? <Counter>{count.adult}</Counter> : <Counter>{count.child}</Counter>}
+          {index === 1 ? (
+            <Counter style={{ display: 'none' }} />
+          ) : item === ADULT ? (
+            <Counter>{count}</Counter>
+          ) : (
+            <Counter>{count}</Counter>
+          )}
         </>
       ))}
     </>
@@ -43,13 +47,9 @@ export const ButtonGroup = [
   { key: 'increase', value: <AddIcon style={{ width: '10px', height: '10px' }} /> },
 ];
 
-const ButtonGroups = styled.span`
-  &:first-of-type {
-    background-color: red;
-  }
-`;
+const ButtonGroups = styled.span``;
 
-const Button = styled.button`
+const Button = styled.button<{ disabled?: boolean }>`
   background-color: #fff;
   border-radius: 5px;
   border: 1px solid rgb(104, 104, 136);
@@ -58,6 +58,11 @@ const Button = styled.button`
   color: #000;
   font-size: 14px;
   cursor: pointer;
+  transition: opacity 0.7s;
+  &:first-of-type {
+    background-color: ${({ disabled }) => disabled && '#eaeaea'};
+    opacity: ${({ disabled }) => disabled && '0.5'};
+  }
 `;
 const Counter = styled.div`
   width: 1.5rem;
