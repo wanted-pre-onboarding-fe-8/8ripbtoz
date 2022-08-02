bd313f42e7710b152e26c5594b6f09b6aebecf03
salangdung-i <dladmswl92@naver.com>
2022-08-03 08:22:11 +0900

    feat: 타입 변경

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,21 +1,15 @@
 export interface IHotel {
+  id: number;
   hotelName: string;
   base: number;
   max: number;
   reservation: boolean;
-  reservationObject?: IReservation;
 }
 
 export type IHotels = IHotel[];
 
-export interface IReservation {
-  id?: number;
-  weekDay?: string;
-  date?: string;
-}
-
 export interface IPayload {
-  hotelName: string;
-  max: number;
+  hotelName?: string;
+  max?: number;
   reservation?: boolean;
 }
