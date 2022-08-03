b23859fba000716b5f4cd4cc07ad94462e300076
corgi-world <developer.msw@gmail.com>
2022-08-03 15:36:50 +0900

    refactor: 팝업을 띄우는 역할을 외부로 위임

--- src/pages/main/search/Popup.tsx
+++ src/pages/main/search/Popup.tsx
@@ -17,28 +17,20 @@ export function usePopup() {
 interface IPopup {
   top: number;
   left: number;
-  isOpen: boolean;
   close: () => void;
   children: JSX.Element;
 }
 
-export function Popup({ top, left, isOpen, close, children }: IPopup) {
+export function Popup({ top, left, close, children }: IPopup) {
   const wrapperRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
-    if (isOpen && wrapperRef.current) {
+    if (wrapperRef.current) {
       wrapperRef.current.focus();
     }
-  }, [isOpen]);
+  }, []);
 
   return (
-    <Wrapper
-      ref={wrapperRef}
-      tabIndex={0}
-      top={top}
-      left={left}
-      isOpen={isOpen}
-      onBlur={() => close()}
-    >
+    <Wrapper ref={wrapperRef} tabIndex={0} top={top} left={left} onBlur={() => close()}>
       {children}
     </Wrapper>
   );
@@ -47,9 +39,7 @@ export function Popup({ top, left, isOpen, close, children }: IPopup) {
 const Wrapper = styled.div<{
   top: number;
   left: number;
-  isOpen: boolean;
 }>`
-  display: ${({ isOpen }) => (isOpen ? 'block' : 'none')};
   position: absolute;
   top: ${({ top }) => top};
   left: ${({ left }) => left};
