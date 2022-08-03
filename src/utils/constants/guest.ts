8167fae98f89faebe264fd943d9a8dc590dd686f
corgi-world <developer.msw@gmail.com>
2022-08-03 14:42:29 +0900

    refactor: guest 상수 기본값 영어로 변경 후 한글도 추가

--- src/utils/constants/guest.ts
+++ src/utils/constants/guest.ts
@@ -1,6 +1,8 @@
 export const GUEST = {
-  ADULT: '성인',
-  CHILD: '아이',
+  ADULT: 'adult',
+  CHILD: 'child',
+  ADULT_KO: '성인',
+  CHILD_KO: '아이',
   INCREASE: 'increase',
   DECREASE: 'decrease',
 };
