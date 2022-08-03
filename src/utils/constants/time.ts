400980428b10a57c17266a6da5b754a03822213a
bebusl <bebus1998@naver.com>
2022-08-03 13:46:17 +0900

    chore: 요일 상수로 따로 분리

--- src/utils/constants/time.ts
+++ src/utils/constants/time.ts
@@ -1,4 +1,5 @@
+const WEEKDAYS = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
 const DISPLAY_WEEKDAYS = ['일', '월', '화', '수', '목', '금', '토'];
 const RESERVATION_MONTH_LIMIT = 12;
 
-export { DISPLAY_WEEKDAYS, RESERVATION_MONTH_LIMIT };
+export { WEEKDAYS, DISPLAY_WEEKDAYS, RESERVATION_MONTH_LIMIT };
