c7d0f616c409604feb07b62d083480a028627b82
ChuYeonBin <cndusqls98@gmail.com>
2022-08-03 11:18:21 +0900

    feat: header bar 구현

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,39 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 
 export default function Header() {
-  return <div>Header</div>;
+  const navigate = useNavigate();
+
+  return (
+    <HeaderWrapper>
+      <Container>
+        <Logo src='../../images/Logo_color.svg' onClick={() => navigate('/')} />
+        <ReservationLink onClick={() => navigate('/reservation')}>예약 내역</ReservationLink>
+      </Container>
+    </HeaderWrapper>
+  );
 }
+
+const HeaderWrapper = styled.header`
+  height: 84px;
+`;
+
+const Container = styled.div`
+  display: flex;
+  height: 100%;
+  align-items: center;
+  justify-content: space-between;
+  margin: 0 auto;
+  max-width: 978px;
+  font-weight: bold;
+`;
+
+const Logo = styled.img`
+  cursor: pointer;
+`;
+
+const ReservationLink = styled.a`
+  font-weight: 600;
+  cursor: pointer;
+`;
