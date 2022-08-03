f37f632e145704fc7de86e1645aaef5108f8667d
salangdung-i <dladmswl92@naver.com>
2022-08-04 08:41:38 +0900

    feat: InfiniteScroll 구현

--- src/queries/hotel.ts
+++ src/queries/hotel.ts
@@ -1,5 +1,5 @@
 import httpClient from '../http';
-import { useQuery, useMutation } from '@tanstack/react-query';
+import { useQuery, useMutation, useInfiniteQuery } from '@tanstack/react-query';
 import { IHotels, ISearchPayload } from '../types/index';
 
 export function getHotels(payload: ISearchPayload) {
@@ -11,4 +11,18 @@ export function getHotels(payload: ISearchPayload) {
 
 export function setReservationHotel() {
   return useMutation((id: number) => httpClient.patch(`/hotels/${id}`, { reservation: true }));
+}
+
+export function getInfiniteScroll(payload: ISearchPayload) {
+  const query = `?hotelName_like=${payload.hotelName}&max_gte=${payload.max}&_limit=10&_page=`;
+  return useInfiniteQuery(
+    ['hotels', payload.hotelName, payload.max],
+    async ({ pageParam = 1 }) => {
+      const res = await httpClient.get<IHotels>(`/hotels${query}` + pageParam);
+      return { data: res, pageParam: pageParam + 1 };
+    },
+    {
+      getNextPageParam: (lastPage) => lastPage.pageParam ?? undefined,
+    },
+  );
 }
