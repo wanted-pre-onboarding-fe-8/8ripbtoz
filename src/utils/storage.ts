b2c2a3328fd8ff878fa7692befb06be676f2a3d9
bebusl <bebus1998@naver.com>
2022-08-03 13:47:17 +0900

    feat: 로컬스토리지 다루는 유틸 함수 구현

---
+++ src/utils/storage.ts
@@ -0,0 +1,39 @@
+import { IHotel } from '../types';
+import { generateReservationDateInfo } from './date';
+
+function getStorageItem(key: string, defaultValue: string) {
+  try {
+    const value = localStorage.getItem(key);
+    if (!value || value === 'null') {
+      return defaultValue;
+    }
+    return value;
+  } catch {
+    return defaultValue;
+  }
+}
+
+export function setHotelItem({ hotelName, id }: IHotel) {
+  const reservation = generateReservationDateInfo();
+  setStorageItem(hotelName, {
+    id,
+    reservation,
+  });
+}
+
+function setStorageItem(key: string, value: string | Record<string, unknown>) {
+  try {
+    if (typeof value === 'object') value = JSON.stringify(value);
+    localStorage.setItem(key, value);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+export function clearStorage() {
+  try {
+    localStorage.clear();
+  } catch (e) {
+    console.log(e);
+  }
+}
