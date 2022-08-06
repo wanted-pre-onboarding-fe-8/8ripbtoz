7ea61eb9f432d242f0b74eeb77f832b1764bc3a1
bebusl <bebus1998@naver.com>
2022-08-06 09:13:10 +0900

    feat: 포커스됐을 때 refetch되는 옵션 끔

--- src/queries/hotel.ts
+++ src/queries/hotel.ts
@@ -24,6 +24,7 @@ export function getInfiniteScroll(payload: ISearchPayload) {
     },
     {
       getNextPageParam: (lastPage) => lastPage.pageParam ?? undefined,
+      refetchOnWindowFocus: false,
     },
   );
 }
